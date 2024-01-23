7839dee4075a52b71bed5015843a770a7d913a50
kristianfiliposki <krisplay@outlook.it>
2024-01-23 22:58:58 +0100

    first commit

---
+++ laravel/resources/views/welcome.blade.php
@@ -0,0 +1,7 @@
+    @extends('layout')
+    @section('content')
+        <h1>home</h1>
+        <p>ciao benvento nel nostro sito</p>
+        <a href="film">film</a>
+    @endsection
+    @section('film')
---
+++ laravel/routes/web.php
@@ -0,0 +1,35 @@
+<?php
+
+
+Route::get('/', function () {
+    $dati=[
+        "movie"=>[
+            "joker",
+            "fliston",
+            "sragonball",
+            "pokemon",
+            "richie richie",
+            "came back to te future",
+
+        ],
+    ];
+
+    return view('welcome');
+});
+
+Route::get('film', function () {
+    $dati=[
+        "movies"=>[
+            "joker",
+            "fliston",
+            "sragonball",
+            "pokemon",
+            "richie richie",
+            "came back to te future",
+
+        ],
+    ];
+
+    return view('film',$dati);
+});
+
